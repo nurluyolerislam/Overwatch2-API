4ab9b6dc6ee577b0b8897a69fc0b978a589abe1d
Erislam Nurluyol <nurluyolerislam@gmail.com>
2024-03-11 06:08:31 +0300

    Initial Commit

---
+++ Overwatch2 API/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Overwatch2 API
+//
+//  Created by Erislam Nurluyol on 11.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Overwatch2 API/Overwatch2_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Overwatch2_APIApp.swift
+//  Overwatch2 API
+//
+//  Created by Erislam Nurluyol on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Overwatch2_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
